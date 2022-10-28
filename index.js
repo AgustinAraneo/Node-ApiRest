c33ab1b798dcc02d0c752453af790987eb749987
Agustin <araneoagustin@gmail.com>
2022-10-28 14:42:48 -0300

    Creacion del primer Json

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 const port = 3001;
 
 app.get("/", (req, res) =>{
-  res.send("Hola mi server en Express");
+  res.send("<h1>Hola, estoy haciendo pruebas</h1><p>Agustin</p>");
+});
+
+app.get("/shop", (req, res) =>{
+  res.send("<h1>Agarath Insumos</h1><p>Proximamente...</p>");
+});
+
+app.get("/productos", (req, res) =>{
+  res.json({
+    name: 'Monitor Asus 144hz 24 Pulgadas',
+    price: 65000
+  });
 });
 
 app.listen(port, () =>{
